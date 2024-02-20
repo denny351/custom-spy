2266b3b969989cc225a562cce1a4a63276f31dc6
Denny Park <denny.park93@gmail.com>
2024-02-19 18:20:52 -0800

    add whitelisted hosts for cors

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import userRoutes from "./routes/userRoutes";
 import setRoutes from "./routes/setRoutes";
 import locationRoutes from "./routes/locationRoutes";
@@ -14,13 +14,25 @@ declare global {
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const allowedHosts = process.env.ALLOWED_HOSTS!.split(",");
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedHosts.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use("/users", userRoutes);
 app.use("/sets", setRoutes);
 app.use("/locations", locationRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
